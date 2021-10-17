074d3da5202531d403af969b46e012d1b4016a40
Muhammad Vicky Julianto <91631855+MukitoZ@users.noreply.github.com>
2021-10-17 04:03:49 +0000

    -Improvement in the code -adding feature to play again -adding medium difficulty -clear the screen after the game restart

--- main.py
+++ main.py
@@ -1,49 +1,60 @@
 from art import logo
 from random import randint
-print(logo)
-random_number = randint(1, 100)
+from replit import clear
 
-def compare(guess_number):
-    global random_number
+#constant variable for attempt
+EASY_DIFFICULTY = 10 
+MEDIUM_DIFFICULTY = 7
+HARD_DIFFICULTY = 5
+
+def compare(random_number, guess_number, lives):
+    """Comparing the random_number and guess_number to check if the guess_number is correct or not"""
     if random_number > guess_number:
-        return "Too low."
+        print("Too low.")
+        return lives - 1
     elif random_number < guess_number:
-        return "Too high."
+        print("Too high.")
+        return lives - 1
     else:
-        return f"You got it! The answer was {guess_number}"
+        print(f"You got it! The answer was {guess_number}")
+
+def attempt():
+    difficulty = input("Choose a difficulty. Type 'easy', 'medium', or 'hard': ").lower()
+    if difficulty == "easy":
+        return EASY_DIFFICULTY
+    elif difficulty == "medium":
+        return MEDIUM_DIFFICULTY
+    elif difficulty == "hard":
+        return HARD_DIFFICULTY
+    else:
+        print("Choose the right difficulty!")
+        return attempt()
+
+def play_again():
+    if input("Do you want to play again? 'y' or 'n': ").lower() == 'y':
+        clear()
+        play_game()
+    else:
+        return
 
 def play_game():
-    global random_number
+    print(logo)
     print("Welcome to the Number Guessing Game!")
     print ("I'm thinking of a number between 1 and 100.")
-    print(f"Pssst, the correct answer is {random_number}")
+    random_number = randint(1, 100)
 
-    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-    if difficulty == "easy":
-        attempt = 10
-        while attempt > 0 :
-            print(f"You have {attempt} attempts remaining to guess the number.")
-            guess_number = int(input("Make a guess: "))
-            condition = compare(guess_number)
-            print(condition)
-            if condition == f"You got it! The answer was {guess_number}":
-                attempt = 0
-            attempt -= 1
-            if attempt > 0:
-                print("Guess again.")
-        print("You've run out of guesses, you lose.")
-    elif difficulty == "hard":
-        attempt = 5
-        while attempt > 0 :
-            print(f"You have {attempt} attempts remaining to guess the number.")
-            guess_number = int(input("Make a guess: "))
-            condition = compare(guess_number)
-            print(condition)
-            if condition == f"You got it! The answer was {guess_number}":
-                attempt = 0
-            attempt -= 1
-            if attempt > 0:
-                print("Guess again.")
-        print("You've run out of guesses, you lose.")
-                
+    guess_number = 0
+    lives = attempt()
+    while guess_number != random_number:
+        print(f"You have {lives} attempts remaining to guess the number.")
+        guess_number = int(input("Make a guess: "))
+        lives = compare(random_number, guess_number, lives)
+        if lives == 0:
+            print("You've run out of guesses, you lose.")
+            play_again()
+            return
+        elif guess_number != random_number:
+            print("Guess again.")
+
+
 play_game()
